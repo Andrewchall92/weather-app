d2818126e82391b51a2289810c2bfb33a1adcb54
moxnix <andrewchall92@gmail.com>
2023-10-05 23:10:06 -0700

    I added the inital html/js/css files and started the html framework

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+// Create a search bar cities
+// Card display for current weather information
+// Card display for 5 day forcast
+// Search history that when clicked brings up the current weather info for selected city
+
+//https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+//https://api.openweathermap.org/data/2.5/weather?q={city name},{country code}&appid={API key}
+//https://api.openweathermap.org/data/2.5/weather?q={city name},{state code},{country code}&appid={API key}
+//http://api.openweathermap.org/geo/1.0/reverse?lat=51.5098&lon=-0.1180&limit=5&appid={API key}
+
+var APIKey = 'be3b0d12ee0e1ba5fd2df576d75ad5b5';
+var city;
+var state;
+var country;
