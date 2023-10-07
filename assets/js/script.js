db587553947e70bc3331b4a1d791c0cc15550129
moxnix <andrewchall92@gmail.com>
2023-10-06 23:26:02 -0700

    I set up an event listener for the search button and then captured its value

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,22 @@
 // Card display for current weather information
 // Card display for 5 day forcast
 // Search history that when clicked brings up the current weather info for selected city
+// Take users input of city name and get coordinates with api call
+// Extract weather data from api call and append to the appropriate spots in current weather
+// Get a five day forcast for city input and append to 5 day forcast cards
+// Store search history in local storage and append each search to a list under the search bar
 
-//https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
-//https://api.openweathermap.org/data/2.5/weather?q={city name},{country code}&appid={API key}
-//https://api.openweathermap.org/data/2.5/weather?q={city name},{state code},{country code}&appid={API key}
-//http://api.openweathermap.org/geo/1.0/reverse?lat=51.5098&lon=-0.1180&limit=5&appid={API key}
+var searchButton = document.querySelector('.search-btn');
+var cityInput = document.querySelector('.city-input');
 
 var APIKey = 'be3b0d12ee0e1ba5fd2df576d75ad5b5';
 var city;
-var state;
-var country;
+var queryURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=5&appid=" + APIKey;
+
+var getCityCoordinates = function() {
+    var cityName = cityInput.value.trim();
+    if (!cityName) return;
+    console.log(cityName)
+}
+searchButton.addEventListener('click', getCityCoordinates);
+
