9ce39aa4e6838ef171c6811456c97de4f32f2e84
moxnix <andrewchall92@gmail.com>
2023-10-09 14:19:15 -0700

    Filtered the forcast data into an array with a 6 day forcast

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,14 +9,38 @@
 
 var searchButton = document.querySelector('.search-btn');
 var cityInput = document.querySelector('.city-input');
-
 var APIKey = 'be3b0d12ee0e1ba5fd2df576d75ad5b5';
+
+var getWeather = function(cityName, lat, lon) {
+var forcastAPI = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey;
+
+fetch(forcastAPI)
+.then(function(response) {
+    return response.json();
+})
+.then(function(data) {
+    var uniqueForecastDays = [];
+    var fiveDaysForecast = [];
+    for (var i = 0; i  < data.list.length; i ++) {
+        var forecast = data.list[i];
+        var forecastDate = new Date(forecast.dt_txt).getDate();
+
+        if(!uniqueForecastDays.includes(forecastDate)) {
+            uniqueForecastDays.push(forecastDate);
+            fiveDaysForecast.push(forecast);
+        }
+    }
+    console.log(fiveDaysForecast);
+    }) .catch(function() {
+    alert("Error occured while fetching weather forcast!")
+    });
+};
 
 
 var getCityCoordinates = function() {
     var cityName = cityInput.value.trim();
     if (!cityName) return;
-    var queryURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=5&appid=" + APIKey;
+    var queryURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=1&appid=" + APIKey;
     console.log(cityName)
 
     fetch(queryURL)
@@ -25,14 +49,18 @@ var getCityCoordinates = function() {
     })
     .then(function(data){
         console.log(data);
-        if(!data.length) {
-            alert("No coordinates found for " + cityName + "!");
+        if(!data.length) return alert("No coordinates found for " + cityName + "!");
             var { name, lat, lon} = data[0];
             getWeather(name, lat, lon);
-        }
+        
     }).catch(function() {
         alert("Can not get corridnates!")
   });
 }
 searchButton.addEventListener('click', getCityCoordinates);
 
+// var fiveDaysForecast = data.list.filter(function(forecast) {
+//     var forecastDate = new Date(forecast.dt_txt).getDate();
+//     if(!uniqueForecastDays.includes(forecastDate)) {
+//         return uniqueForecastDays.push(forecastDate);
+//     }
